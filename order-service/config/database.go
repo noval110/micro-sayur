f656d3eb3a5358483d825f5465b3756520f4886d
rflnhdyt626 <nurhidayatrafli4@gmail.com>
2026-08-18 19:11:38 +0700

    fix: order-service undefined database variable

--- order-service/config/database.go
+++ order-service/config/database.go
@@ -11,13 +11,16 @@ import (
 
 func InitDB() *sql.DB {
 	dsn := os.Getenv("DATABASE_URL")
+	var database string
 	if dsn == "" {
 		host := getEnv("DB_HOST", "127.0.0.1")
 		port := getEnv("DB_PORT", "5432")
 		user := getEnv("DB_USER", "postgres")
 		password := getEnv("DB_PASSWORD", "postgres")
-		database := getEnv("DB_NAME", "sayur_order_db")
+		database = getEnv("DB_NAME", "sayur_order_db")
 		dsn = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, database)
+	} else {
+		database = "PaaS Cloud"
 	}
 
 	db, err := sql.Open("postgres", dsn)
